b92ce0f46c96612388849bdd4bcb0c73fe7678bb
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-06-19 19:52:56 +0200

    Connected everything in app.js

--- app.js
+++ app.js
@@ -1,5 +1,42 @@
 const express = require('express');
+const session = require('express-session');
+const cookieParser = require("cookie-parser");
+
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+// UTILS
+const sequelize = require('./util/database');
+const createSubjects = require('./util/createSubject');
+const createAdmin = require('./util/createAdminUser');
+
+// MODELS
+const Grade = require('./models/grade');
+const Permission = require('./models/permission');
+const User = require('./models/user');
+const Subject = require('./models/subject');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.listen(4000);
+createSubjects();
+createAdmin();
+
+const adminRoutes = require('./routes/admin');
+const loginRoutes = require('./routes/login');
+const recordRoutes = require('./routes/record');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+ app.use(loginRoutes);
+ app.use('/admin', adminRoutes);
+ app.use('/record', recordRoutes);
+
+sequelize.sync()
+.then(result => {
+    app.listen(4000);
+})
+.catch(err => console.error(err));
